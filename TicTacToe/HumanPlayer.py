70bab675c139a9434c5e0d8fe4849a1e850f7189
JD Semrau <jan@ternary.asia>
2020-08-26 15:10:33 +0800

    Initial commit

---
+++ TicTacToe/HumanPlayer.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pickle
+
+
+BOARD_ROWS=3
+BOARD_COLS=3
+
+class HumanPlayer:
+    def __init__(self, name):
+        self.name = name
+
+    def chooseAction(self, positions):
+        while True:
+            row = int(input("Input your action row:"))
+            col = int(input("Input your action col:"))
+            action = (row, col)
+            if action in positions:
+                return action
+
+    # append a hash state
+    def addState(self, state):
+        pass
+
+    # at the end of game, backpropagate and update states value
+    def feedReward(self, reward):
+        pass
+
+    def reset(self):
+        pass
+
+if __name__ == '__main__':
+    print("Initialize Human Player")
